2ab32411c88faa70063aab1a419555513904419e
albertocastro <albertocastro@192.168.1.78>
2018-07-08 17:26:46 -0500

    ESTE TRABAJO ES DEL EJERCICIO UNO SOBRE LISTAS, COMOS Y MODELOS

---
+++ src/tarea1/Tarea1.java
@@ -0,0 +1,21 @@
+/*
+ * Castro Quevedo Luis Alberto
+ */
+package tarea1;
+
+/**
+ *
+ * @author albertocastro
+ */
+public class Tarea1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Vista1 v1=new Vista1();
+        v1.setVisible(true);
+    }
+    
+}
